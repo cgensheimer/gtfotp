f1f95f044524de838c9cc6ea788dce50cc6d56f4
Cole Gensheimer <92688065+cgensheimer@users.noreply.github.com>
2023-11-23 00:39:52 -0500

    initial commit
    default project base

---
+++ gtfotp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gtfotp
+//
+//  Created by Gensheimer, Cole M on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gtfotp/gtfotpApp.swift
@@ -0,0 +1,17 @@
+//
+//  gtfotpApp.swift
+//  gtfotp
+//
+//  Created by Gensheimer, Cole M on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct gtfotpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
